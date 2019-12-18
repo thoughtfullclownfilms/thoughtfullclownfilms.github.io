6b69c855caa2ed212cd0effdbff0882d64fe11bd
Neeraj <sinne07@gmail.com>
2019-12-18 13:42:26 +0530

    first commit

---
+++ src/Containers/Home.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+
+export default function Home() {
+  const [hideLogo, setHideLogo] = useState(false);
+  return (
+    <div>
+      <div
+        hidden={hideLogo}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100%",
+          backgroundColor: "black"
+        }}
+      >
+        <ReactPlayer
+          url="../logoproxy.mp4"
+          playing
+          muted
+          width="88.5%"
+          height="30%"
+          onEnded={e => {
+            setHideLogo(true);
+          }}
+        />
+      </div>
+      <div hidden={!hideLogo}>herre</div>
+    </div>
+  );
+}
